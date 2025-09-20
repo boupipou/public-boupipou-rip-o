527b3ed63cd4adfef236c1cd02536c0db50bdc7f
boupipou <khouloudbenjamin@gmail.com>
2025-09-20 11:47:53 +0200

    moved projects to pvt repo

--- [DEV]PRJ/[PRJ]WebReports/$web/Main/footer.js
+++
@@ -1,8 +0,0 @@
-export async function injectFooter(path = 'footer.html') {
-  try {
-    const html = await fetch(path).then(r => r.text());
-    document.body.insertAdjacentHTML('beforeend', html);
-  } catch (err) {
-    console.error('Footer failed to load:', err);
-  }
-}
